d261d76aa7ab51065b447b02b574bd39e3a6a152
Alessio Proietti <alessio.proietti@protonmail.com>
2022-07-13 15:42:31 +0200

    BytesToBits

--- src/serpentine/CRYSTALS/Kyber/preliminaries/algorithms.py
+++ src/serpentine/CRYSTALS/Kyber/preliminaries/algorithms.py
@@ -1,8 +1,29 @@
-def parse(b):
-    return b
+def Parse(bytes_stream):
+    return bytes_stream
 
-def CBD(b):
-    return b
 
-def decode(b):
-    return b
+def CBD(n, bytes_array):
+    beta = BytesToBits(bytes_array)
+    return bytes_array
+
+
+def Decode(l, bytes_array):
+    beta = BytesToBits(bytes_array)
+    return bytes_array
+
+
+def BytesToBits(bytes_array):
+    list_of_bitlists_strings = [list('{0:08b}'.format(B)) for B in bytes_array]
+    bits_array = [int(bit)
+                  for bitlist in list_of_bitlists_strings for bit in bitlist]
+
+    return bits_array
+
+
+"""
+from sympy import ntt
+
+    q = 3329
+    transform = ntt(byte_stream, q)
+    return transform
+"""
